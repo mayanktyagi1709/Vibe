d2c726676ee3232e489d058f6f454421cfa79b7a
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-06-20 11:21:12 +0530

    Created user list

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,6 +6,8 @@ export {default as TeamChannelPreview} from "./TeamChannelPreview"
 export { default as ChannelInner } from './ChannelInner';
 export { default as CreateChannel } from './CreateChannel';
 export { default as EditChannel } from './EditChannel';
+export { default as UserList } from './UserList';
+
 export {default as Auth} from "./Auth"
 
 
