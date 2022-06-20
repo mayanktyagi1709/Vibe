60b5e4aaf09d07cf9a8baac469d54226804b35e7
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-06-20 09:53:28 +0530

    Created channels section

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import 'stream-chat-react/dist/css/index.css';
 import React, { useState } from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
@@ -11,7 +12,6 @@ const apiId = process.env.STREAM_APP_API;
 const authToken = cookies.get("token");
 
 const client = StreamChat.getInstance(apiKey, apiId);
-
 
 if (authToken) {
   client.connectUser(
@@ -28,12 +28,27 @@ if (authToken) {
 }
 
 function App() {
+  const [createType, setCreateType] = useState("");
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
   if (!authToken) return <Auth />;
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
-        <ChannelListContainer />
-        <ChannelContainer />
+        <ChannelListContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+        <ChannelContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
+        />
       </Chat>
     </div>
   );
